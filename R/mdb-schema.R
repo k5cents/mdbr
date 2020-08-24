bdc036f0888e00f6b70cf556b33f43d5e0141da9
Kiernan Nicholls <kiernann@protonmail.com>
2020-08-24 14:32:02 -0400

    Create mdb_schema()

---
+++ R/mdb-schema.R
@@ -0,0 +1,24 @@
+#' Find the column types of a table
+#'
+#' Used to determine the column types for [read_mdb()].
+#'
+#' @param file Path to the Microsoft Access file.
+#' @param table Name of the table, use `mdb_tables()`.
+#' @return A tibble of table columns and data types.
+#' @importFrom readr read_delim
+mdb_schema <- function(file, table = NULL) {
+  if (is.null(table)) {
+    stop("Must define a table name, see mdb_tables()")
+  }
+  x <- system2(
+    command = "mdb-schema",
+    args = c(file, paste("-T", table)),
+    stdout = TRUE
+  )
+  x <- x[13:(grep("\\);", x) - 1)]
+  x <- gsub("^\\s", "", x)
+  x <- gsub("\t+", ",", x)
+  x <- gsub(",\\s?$", "", x)
+  x <- gsub("\\[|\\]", "", x)
+  readr::read_delim(x, delim = ",", col_names = c("col", "type"))
+}
