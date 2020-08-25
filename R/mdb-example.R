40c3ab3adc90f785addc7f06300b52938c9a1b98
Kiernan Nicholls <kiernann@protonmail.com>
2020-08-25 16:10:21 -0400

    Create mdb_example()

---
+++ R/mdb-example.R
@@ -0,0 +1,15 @@
+#' Get path to mdbr example
+#'
+#' mdbr comes bundled with a sample file from the
+#' [nycflights13](https://github.com/hadley/nycflights13) package in its
+#' inst/extdata directory. This function make it easy to access.
+#'
+#' @param path path to the Microsoft Access file.
+#' @export
+mdb_example <- function(path = "nycflights13.mdb") {
+  if (!is.character(path)) {
+    dir(system.file("extdata", package = "mdbr"))
+  } else {
+    system.file("extdata", path, package = "mdbr", mustWork = TRUE)
+  }
+}
