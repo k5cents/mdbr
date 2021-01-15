25fc74f560e6e77e9b4df06867ff51fca9ed97da
Kiernan Nicholls <kiernann@protonmail.com>
2021-01-15 15:33:23 -0500

    Fix test mdbtools command

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,6 @@
 has_mdb_tools <- function() {
   try <- tryCatch(
-    expr = system2("mdb-tools", stderr = TRUE, stdout = TRUE),
+    expr = system2("mdb-tables", stderr = TRUE, stdout = TRUE),
     error = function(e) return(NULL)
   )
   is.null(try)
