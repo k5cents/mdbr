4776af38a834ac5295c82b8ad36a9d8167f2fa9a
Kiernan Nicholls <kiernann@protonmail.com>
2021-01-18 13:20:27 -0500

    Test export with FALSE column names

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -18,6 +18,19 @@ test_that("tables can be exported to file", {
   expect_equal(file.size(tmp), 450)
 })
 
+test_that("tables can be exported without escape", {
+  skip_on_cran()
+  skip_if_not(has_mdb_tools())
+  dat <- export_mdb(
+    file = mdb_example(),
+    table = "Airlines",
+    output = TRUE,
+    quote_escape = FALSE
+  )
+  expect_type(dat, "character")
+  expect_length(dat, 1)
+})
+
 test_that("exporting errors without table name", {
   skip_on_cran()
   skip_if_not(has_mdb_tools())
