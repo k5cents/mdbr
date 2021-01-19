b024ed85f0ea35d9d7d4f09424b1ea739ccf81bb
Kiernan Nicholls <kiernann@protonmail.com>
2021-01-19 10:58:51 -0500

    Use -1 flag for mdb-tables

--- R/tables.R
+++ R/tables.R
@@ -1,16 +1,13 @@
 #' List tables in a Microsoft Access database
 #'
-#' Used to find table names for `read_mdb()`.
-#'
 #' @param file Path to the Microsoft Access file.
 #' @return A character vector of table names.
 #' @export
 mdb_tables <- function(file) {
   check_mdb_tools()
-  x <- system2(
+  system2(
     command = "mdb-tables",
-    args = c(file, "-d '\n'"),
+    args = c("-1", shQuote(file)),
     stdout = TRUE
   )
-  x[nzchar(x)]
 }
